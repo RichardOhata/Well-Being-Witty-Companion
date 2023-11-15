4c8c1cbae1a2f5eb91720d7076fdccd9135f820c
RichardOhata <richardohata@gmail.com>
2023-11-14 16:11:41 -0800

    Created basic login page

--- Client/JS/login.js
+++ Client/JS/login.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const signupForm = document.getElementById('signupForm')
-    signupForm.addEventListener('submit', (e) => {
+    const loginForm = document.getElementById('loginForm')
+    loginForm.addEventListener('submit', (e) => {
         e.preventDefault()
         const username = document.getElementById('username').value
-        const email = document.getElementById('email').value
         const password = document.getElementById('password').value
         post({
             username: username,
-            email: email,
             password: password
         })
     });
