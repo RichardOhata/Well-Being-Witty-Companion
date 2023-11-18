05162467ac038d7db31784c8c49017319fcaa67d
Thant Yati Thaw <tthaw@my.bcit.ca>
2023-11-18 00:51:13 -0800

    Testing dadsaysjokes model with pipeline w/o downloading

--- Server/landing.py
+++ Server/landing.py
@@ -1,26 +1,24 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from transformers import OpenAIGPTTokenizer, OpenAIGPTLMHeadModel
-import torch
+from transformers import pipeline
 
 app = Flask(__name__)
 CORS(app)
 
-# Load the model and tokenizer
-model_name = "openai-gpt"
-tokenizer = OpenAIGPTTokenizer.from_pretrained(model_name)
-model = OpenAIGPTLMHeadModel.from_pretrained(model_name)
+# Load the text generation pipeline with the specified model
+generator = pipeline('text-generation', model='huggingtweets/dadsaysjokes')
 
 @app.route('/get-joke', methods=['POST'])
 def get_joke():
     data = request.json
     age = data['age']
-    joke_query = f"Tell me a joke suitable for someone aged {age}."
+    joke_query = f"A short joke for {age} year old age:"
 
-    # Generate a response using the model
-    inputs = tokenizer.encode(joke_query, return_tensors="pt")
-    outputs = model.generate(inputs, max_length=50)  # Adjust max_length as needed
-    joke_response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    # Generate a response using the pipeline
+    jokes = generator(joke_query, num_return_sequences=1)
+
+    # The pipeline returns a list of dictionaries, extract the generated text
+    joke_response = jokes[0]['generated_text']
 
     return jsonify(joke=joke_response)
 
