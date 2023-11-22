9cddac77d76673909bc718ebfc97b7ef577d6c7c
Thant Yati Thaw <tthaw@my.bcit.ca>
2023-11-21 21:36:42 -0800

    health advice and short jokes work but not the models

--- Server/landing.py
+++ Server/landing.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from transformers import pipeline
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 app = Flask(__name__)
 CORS(app)
@@ -8,19 +9,37 @@ CORS(app)
 # Load the text generation pipeline with the specified model
 generator = pipeline('text-generation', model='huggingtweets/dadsaysjokes')
 
+model_name = "vicgalle/gpt2-alpaca"
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+model = GPT2LMHeadModel.from_pretrained(model_name)
+
+
 @app.route('/get-joke', methods=['POST'])
 def get_joke():
     data = request.json
     age = data['age']
     joke_query = f"A short joke for {age} year old age:"
-
-    # Generate a response using the pipeline
     jokes = generator(joke_query, num_return_sequences=1)
-
-    # The pipeline returns a list of dictionaries, extract the generated text
     joke_response = jokes[0]['generated_text']
 
     return jsonify(joke=joke_response)
+
+@app.route('/get-health-advice', methods=['POST'])
+def get_health_tip():
+    data = request.json
+    age = data['age']
+    health_tip_query = f"A health tip for {age} year olds:"
+    print(health_tip_query)
+
+    # Encode the input text and generate a health tip
+    input_ids = tokenizer.encode(health_tip_query, return_tensors="pt")
+    output = model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+    health_tip_response = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    # Limit the response to a single line
+    health_tip_response = health_tip_response.split('\n')[0]
+
+    return jsonify(advice=health_tip_response)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app.js
+++ app.js
@@ -14,19 +14,19 @@ app.use(cookieParser());
 // const secretKey = require('crypto').randomBytes(64).toString('hex') 
 const secretKey = "---" 
 
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER, 
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE,
-// });
-
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '-',
-    database: '-'
-}); 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '-',
+//     database: '-'
+// }); 
 db.connect((err) => {
     if (err) {
         console.error('Error connecting to the database:', err);
